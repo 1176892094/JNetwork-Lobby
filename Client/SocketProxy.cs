2e5852fbb7b496238eec7600da791dbe2d7239db
Charlotte <1176892094@qq.com>
2024-04-24 17:44:29 +0800

    Update SocketProxy.cs

--- Client/SocketProxy.cs
+++ Client/SocketProxy.cs
@@ -29,12 +29,7 @@ namespace JFramework.Net
         /// <summary>
         /// 接收端口
         /// </summary>
-        private IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.IPv6Any, 0);
-
-        /// <summary>
-        /// 接收事件
-        /// </summary>
-        public event Action<IPEndPoint, byte[]> OnReceive;
+        private IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
         /// <summary>
         /// 距离上次交互的时间
@@ -45,35 +40,54 @@ namespace JFramework.Net
         /// 这是一个构造函数，初始化一个指向特定远程端点的UdpClient。在UdpClient上启动接收并设置最后交互时间。
         /// </summary>
         /// <param name="port"></param>
+        /// <param name="OnReceive"></param>
         /// <param name="endPoint"></param>
-        public SocketProxy(int port, IPEndPoint endPoint)
+        public SocketProxy(int port, Action<IPEndPoint, byte[]> OnReceive, IPEndPoint endPoint)
         {
             punchClient = new UdpClient();
-            punchClient.Connect(new IPEndPoint(IPAddress.IPv6Loopback, port));
-            punchClient.BeginReceive(ReceiveData, punchClient);
+            punchClient.Connect(new IPEndPoint(IPAddress.Loopback, port));
+            punchClient.BeginReceive(ServerReceive, punchClient);
             remoteEndPoint = new IPEndPoint(endPoint.Address, endPoint.Port);
             dateTime = DateTime.Now;
+
+            void ServerReceive(IAsyncResult result)
+            {
+                var data = punchClient.EndReceive(result, ref clientEndPoint);
+                punchClient.BeginReceive(ServerReceive, punchClient);
+                isActive = true;
+                dateTime = DateTime.Now;
+                OnReceive?.Invoke(remoteEndPoint, data);
+            }
         }
 
         /// <summary>
         /// 这也是一个构造函数，它初始化一个监听特定端口的UdpClient。
         /// </summary>
         /// <param name="port"></param>
-        public SocketProxy(int port)
+        /// <param name="OnReceive"></param>
+        public SocketProxy(int port, Action<byte[]> OnReceive)
         {
             punchClient = new UdpClient(port);
-            punchClient.BeginReceive(ReceiveData, punchClient);
+            punchClient.BeginReceive(ClientReceive, punchClient);
             dateTime = DateTime.Now;
+
+            void ClientReceive(IAsyncResult result)
+            {
+                var data = punchClient.EndReceive(result, ref clientEndPoint);
+                punchClient.BeginReceive(ClientReceive, punchClient);
+                isActive = true;
+                dateTime = DateTime.Now;
+                OnReceive?.Invoke(data);
+            }
         }
 
         /// <summary>
         /// 这个方法将一组数据发送给在构造函数中定义好的远程端点。
         /// </summary>
         /// <param name="data"></param>
-        /// <param name="length"></param>
-        public void ServerSend(byte[] data, int length)
+        public void ServerSend(byte[] data)
         {
-            punchClient.Send(data, length);
+            punchClient.Send(data, data.Length);
             dateTime = DateTime.Now;
         }
 
@@ -81,27 +95,13 @@ namespace JFramework.Net
         /// 这个方法只有在客户端完成初始接收后才发送数据。它将数据发送到最近收到的数据包的来源。
         /// </summary>
         /// <param name="data"></param>
-        /// <param name="length"></param>
-        public void ClientSend(byte[] data, int length)
+        public void ClientSend(byte[] data)
         {
             if (isActive)
             {
-                punchClient.Send(data, length, clientEndPoint);
+                punchClient.Send(data, data.Length, clientEndPoint);
                 dateTime = DateTime.Now;
             }
-        }
-
-        /// <summary>
-        /// 这是一个私有方法，用于处理接收到的数据。它读取数据，重新开始接收，并触发dataReceived事件。
-        /// </summary>
-        /// <param name="result"></param>
-        private void ReceiveData(IAsyncResult result)
-        {
-            var data = punchClient.EndReceive(result, ref clientEndPoint);
-            punchClient.BeginReceive(ReceiveData, punchClient);
-            isActive = true;
-            dateTime = DateTime.Now;
-            OnReceive?.Invoke(remoteEndPoint, data);
         }
 
         /// <summary>
@@ -111,7 +111,6 @@ namespace JFramework.Net
         {
             punchClient.Dispose();
             clientEndPoint = null;
-            OnReceive = null;
         }
     }
 }
