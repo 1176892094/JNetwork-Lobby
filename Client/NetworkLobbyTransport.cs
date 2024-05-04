7d059a10fe485eebc9a60a6baa8706712e383ad0
Charlotte <1176892094@qq.com>
2024-05-04 12:51:23 +0800

    Update NetworkLobbyTransport.cs

--- Client/NetworkLobbyTransport.cs
+++ Client/NetworkLobbyTransport.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using JFramework;
 using JFramework.Core;
 using JFramework.Interface;
 using UnityEngine;
